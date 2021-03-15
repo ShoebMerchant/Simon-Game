1368cc6544c0e5981725ad756f3b5817ff6c36e9
ShoebMerchant <66074190+ShoebMerchant@users.noreply.github.com>
2021-03-16 01:08:30 +0530

    Update game.js

--- game.js
+++ game.js
@@ -61,7 +61,7 @@ function checkAnswer(currenLevel){
         setTimeout(function(){
             $("body").removeClass("game-over");
         }, 200);
-        $("h1").text("Game Over, Press Any Key to Restart");
+        $("#level-title").text("Game Over, Press Any Key to Restart");
         startOver();
     }
 }
